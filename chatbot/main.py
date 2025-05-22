1141875861d690742114703239455918491e5b5f
Hamza724x <hamzanawaz78613@gmail.com>
2025-05-22 20:53:26 +0100

    Uploading the chatbot that I had created and submitted for the bootcamp assignment.

---
+++ chatbot/main.py
@@ -0,0 +1,37 @@
+import openai
+import os
+
+openai.api_key = "OpenAI_API_Key"
+
+class virtualAssisstant:
+    def __init__(self):
+        pass
+
+    def chat_with_gpt(self, prompt):
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": prompt}],
+        )
+        return response['choices'][0]['message']['content'].strip()
+    
+    def get_response(self, user_input):
+        return self.chat_with_gpt(user_input)
+
+
+def chat():
+    print("Welcome to Hamza Virtual Assistant!")
+    print("To Exit, type 'exit' \n")
+
+    myChatbot = virtualAssisstant()
+    
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() == "exit":
+            print("Thank you for using Hamza Virtual Assistant, Goodbye!")
+            break
+        response = myChatbot.get_response(user_input)
+        print("Chatbot: ", response)
+
+if __name__ == "__main__":
+    chat()
+
